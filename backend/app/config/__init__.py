33261dc976bb8a03c2e5aaa20008493809d3a652
PauloCCB_03 <paulo.calderon@comprafacil-usa.com>
2025-12-04 09:32:25 -0500

    feat : Se quita comentanrios innecesarios

--- backend/app/config/__init__.py
+++ backend/app/config/__init__.py
@@ -1,14 +1,6 @@
 """
-<<<<<<< HEAD
 Módulo de configuración de la aplicación.
-"""
-from app.config.settings import settings
-
-__all__ = ["settings"]
-=======
-Módulo de configuración.
 """
 from .settings import get_settings
 
 __all__ = ["get_settings"]
->>>>>>> main
